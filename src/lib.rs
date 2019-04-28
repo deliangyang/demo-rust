d82513aadde51c948e3d5ad5b35426f7cc411d83
ydl <yangdeliang@juwang.cn>
2019-04-28 21:51:55 +0800

    feat: lifetime

--- src/lib.rs
+++ src/lib.rs
@@ -2,3 +2,11 @@
 pub fn add(a: i32, b: i32) -> i32 {
     a + b
 }
+
+pub fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
+    if x.len() > y.len() {
+        x
+    } else {
+        y
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,39 @@
+extern crate regex;
+
+use regex::Regex;
+
 pub mod lib;
 
 fn main() {
+
+    let reg = Regex::new(r"(\d{4})-(\d{2})-(\d{2})").unwrap();
+    let caps = reg.captures("2019-04-28").unwrap();
+    println!("{}", caps.get(1).unwrap().as_str());
+    println!("{}", caps.get(2).unwrap().as_str());
+    println!("{}", caps.get(3).unwrap().as_str());
     println!("a + b = {}", lib::add(2, 3));
+
+
+    let test = Test {
+        a: String::from("a")
+    };
+    test.test();
+
+    println!("the longest is: {}", lib::longest("abc", "a"));
 }
+
+trait Default {
+    fn test(&self);
+}
+
+struct Test {
+    a: String
+}
+
+impl Default for Test {
+    fn test(&self) {
+        println!("Hello world {}", self.a);
+    }
+}
+
 
